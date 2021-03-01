a72ebc82bbd60c54213bf60c32027ca65b578789
jrmthbt <jrmthbt@hotmail.com>
2021-03-01 16:43:50 +0100

    config for sass compress css min

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import {terser} from "rollup-plugin-terser";
+
 export default {
-    input: 'js/app.js' ,
-    output: {
-        file: 'dist/js/app.js',
-        format: 'iife'
-    },
-};
+    input: ['js/app.js'],
+    output: [
+        {file: "dist/js/app.js", format: "iife"},
+        {file: "dist/min/js/app.min.js", format: "iife", plugins: [terser ()]}
+
+    ],
+}
