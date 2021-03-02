0f163edcbbd31f3191769dd58ec60c81cd1c3507
jrmthbt <jrmthbt@hotmail.com>
2021-03-02 16:38:30 +0100

    crudJS test

---
+++ js/composents/magasin.mjs
@@ -0,0 +1,28 @@
+export const questions = [
+    {
+        index : 0,
+        type : "Qcm",
+        enonce : "J'espère que vous [%R1]viendrez[/%R1] à notre fête",
+        choix : "choix",
+        BonneReponse : "viendrez",
+        explication : ""
+
+    },
+    {
+        index : 1,
+        type : "identification",
+        enonce : "[%R1]Tout[/%R1] est parfait ",
+        choix : "",
+        BonneReponse : "",
+        explication : "Tout n'est pas un verbe"
+    },
+    {
+        index : 2,
+        type : "réponse courte",
+        enonce : "Je [%R1]être[/%R1]désolée",
+        choix : "",
+        BonneReponse : "suis",
+        explication : "1ière personne du singulier"
+
+    }
+]
