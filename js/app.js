47cebc1a693e47b2cb32d252cfe6e8488bd63175
jrmthbt <jrmthbt@hotmail.com>
2021-03-22 15:20:13 +0100

    Active/DesactiveLS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
+
 import {Model} from "./composents/model.mjs";
 import {View} from "./composents/view.mjs";
 import {Controller} from "./composents/controller.mjs";
+import {getTagsy, callLS, stopLS} from "./composents/API/LocalStorage.mjs";
 
 
 let app = new Controller(new Model(), new View());
 
 console.table(app.model.qcmAnswers);
 console.table(app.model.shortAnswers);
+
+if (document.getElementById("save-info").checked === true){
+    callLS();
+
+}else{
+   document.getElementById("save-info").addEventListener("change", function(){
+       if (this.checked){
+           callLS();
+       }else{
+           stopLS();
+       }
+   })
+}
+
+document.body.onload = getTagsy;
---
+++ js/composents/API/LocalStorage.mjs
@@ -0,0 +1,49 @@
+ export const tagsy = {
+    "autoSave" : document.getElementById("save-info"),
+    "counterAuto" : document.getElementById("counter"),
+    "exerciceName" : document.getElementById("name-exercise"),
+};
+
+ export let saveTagsy = (tagsy)=> {
+    if (typeof localStorage === "undefined" && JSON) {
+        document.querySelector('#time-save').innerHTML = "Sauvegarde automatique indisponible";
+    }
+
+    let tagsyData = {
+        'autoSave' : tagsy.autoSave.checked,
+        'counterAuto' : tagsy.counterAuto.checked,
+        'exerciseName' : tagsy.exerciceName.value
+    }
+
+    console.log(tagsyData);
+
+    let store = JSON.stringify(tagsyData);
+    localStorage.setItem("saveTagsy", store);
+
+
+}
+
+ export let getTagsy = () => {
+   const getData = JSON.parse(localStorage.getItem('saveTagsy'));
+
+   document.getElementById("save-info").checked = getData.autoSave;
+    document.getElementById("counter").checked = getData.counterAuto;
+    document.getElementById("name-exercise").value = getData.exerciseName;
+}
+
+export let callLS =() => {
+    document.querySelectorAll("input").forEach(input => {
+        input.addEventListener("focusout", save)
+    })
+}
+
+export let stopLS = ()=>{
+    document.querySelectorAll("input").forEach(input => {
+        input.removeEventListener("focusout", save);
+
+    })
+}
+
+let save = () => {
+     saveTagsy(tagsy);
+}
